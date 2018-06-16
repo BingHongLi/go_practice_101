64bcd94a40062a8686451600fd5266290a9b50c1
binghongli <b97607065@gmail.com>
2018-06-17 02:03:03 +0800

    北京798的旅行

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+
+# SET THE FOLLOWING VARIABLES
+# docker hub username
+USERNAME=yunabe
+# image name
+IMAGE=lgo
+version=20180525
+
+create_go_jupyter()
+{
+docker run --name go-jupyter -p 8888:8888 -p 80:5000 -v $(pwd):/home/gopher -d $USERNAME/$IMAGE:$version jupyter notebook --ip=0.0.0.0 --NotebookApp.token=''
+}
+
+# 必須偵測container是否已經存在，進而做判斷
+if [ `docker ps -a |grep go-jupyter | wc -l`  =  0  ]; then
+  create_go_jupyter
+else
+  docker rm -f go-jupyter
+  create_go_jupyter
+fi
