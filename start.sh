cfc06965f9a02884cf8190617dbedc2fc01a8397
binghongli <b97607065@gmail.com>
2018-06-17 02:16:34 +0800

    modify command

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ version=20180525
 create_go_jupyter()
 {
 docker run --name go-jupyter -p 8888:8888 -p 80:5000 -v $(pwd)/material:/examples -d $USERNAME/$IMAGE:$version jupyter notebook --ip=0.0.0.0 --NotebookApp.token=''
-docker exec go-jupyter cp -r /examples/* /home/gopher/
+docker exec go-jupyter cp -r /examples/ /home/gopher/
 }
 
 # 必須偵測container是否已經存在，進而做判斷
